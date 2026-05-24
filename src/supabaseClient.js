d60efb73066aff78ef4646d9b20288252b821679
Panuwat <in.klang2551@gmail.com>
2026-05-24 16:46:22 +0700

    update: fixed bug 6

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,10 @@
+
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+/*const supabaseUrl = "https://anxpshgcdxeocwqryjrp.supabase.co";
+const supabaseAnonKey = "sb_publishable_t1NZI00h01h3c21Y5oxM2A_TInArZSa";*/
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
