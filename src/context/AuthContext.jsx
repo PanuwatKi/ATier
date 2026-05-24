ae8874f450441c2a62bb8d504162444979911815
Panuwat <in.klang2551@gmail.com>
2026-05-24 15:31:11 +0700

    update: fixed bug 3

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -69,7 +69,7 @@ export function AuthProvider({ children }) {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: 'https://atier-web.vercel.app/', // ตรวจสอบให้ตรงกับ URL เว็บของคุณ
+        redirectTo: window.location.origin, // ตรวจสอบให้ตรงกับ URL เว็บของคุณ
       },
     });
     if (error) console.error("Error logging in with Google:", error.message);
